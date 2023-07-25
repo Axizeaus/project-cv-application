59fbe38b62180d04edbc56f1588a4a449964a111
Axizeaus <axizeaus@gmail.com>
2023-07-25 13:45:51 +0630

    general info working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import GeneralInfo from "./components/General";
+
+const dummyData = {
+  name: "Joe Schmoe",
+  email: "joe@schomoe.com",
+  phone: "0123 888 654 3214",
+};
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState(dummyData);
+
+  console.log(personalInfo);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPersonalInfo((prevInfo) => {
+      return {
+        ...prevInfo,
+        [name]: value,
+      };
+    });
+  };
 
   return (
     <>
-      <h1>Hello there</h1>
+      <GeneralInfo info={personalInfo} handleChange={handleChange} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
